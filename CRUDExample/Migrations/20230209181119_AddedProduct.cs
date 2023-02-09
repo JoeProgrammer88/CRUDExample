28e43cba1ac359da81830401e05081f92d5e7297
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2023-02-09 10:12:52 -0800

    Ran Add-Migration to create code first migration to add Phone model into database

---
+++ CRUDExample/Migrations/20230209181119_AddedProduct.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDExample.Migrations
+{
+    public partial class AddedProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phone",
+                columns: table => new
+                {
+                    SerialNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Size = table.Column<double>(type: "float", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phone", x => x.SerialNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phone");
+        }
+    }
+}
